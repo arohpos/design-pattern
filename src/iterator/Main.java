6f60a8eb26ced3b31cf2d16bf043d7a9e9680c79
shushushun <103364520+shushushun@users.noreply.github.com>
2022-09-11 17:38:10 +0900

    add: append one another book into BookShelf
    to check whether BookShelf class defined by ArrayList works

--- src/iterator/Main.java
+++ src/iterator/Main.java
@@ -6,19 +6,20 @@ public class Main{
 	
 	public static void main(String[] args) {
 		
-		BookShelf bookShelf = new BookShelf(4);
+		BookShelf bookShelf = new BookShelf(4);		
 		bookShelf.appendBook(new Book("Around the World in 80 Days"));
 		bookShelf.appendBook(new Book("Bible"));
 		bookShelf.appendBook(new Book("Cinderella"));
 		bookShelf.appendBook(new Book("Daddy-Long-Legs"));
+		bookShelf.appendBook(new Book("East of Eden"));
 		
 		Iterator<Book> it = bookShelf.iterator();
+		
 		while(it.hasNext()) {
 			Book book = it.next();
 			System.out.println(book.getName());
 		}
 		System.out.println();
-		
 	}
 	
 }
