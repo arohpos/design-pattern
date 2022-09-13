e46391525d0b3265c75b724c4d11427504af934a
arohpos <110832649+arohpos@users.noreply.github.com>
2022-09-13 22:47:08 +0900

    add: add another way to construct Adapter
    not by using inheritance but by using delegation

--- src/main/adapter/Print.java
+++ src/main/adapter/Print.java
@@ -1,6 +1,16 @@
+//継承によるadapter
+/*
 package main.adapter;
 
 public interface Print {
+	public abstract void printWeak();
+	public abstract void printStrong();
+}*/
+
+//委譲によるadapter
+package main.adapter;
+
+public abstract class Print {
 	public abstract void printWeak();
 	public abstract void printStrong();
 }
