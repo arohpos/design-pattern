5b098cafc94ca8d6f25ee5e8ed4e7d1b1bad84f6
arohpos <110832649+arohpos@users.noreply.github.com>
2022-09-13 22:49:10 +0900

    add: add another way to construct Adapter
    not by using inheritance but by using delegation

--- src/main/adapter/PrintBanner.java
+++ src/main/adapter/PrintBanner.java
@@ -1,6 +1,9 @@
+//継承によるadapter
+/*
 package main.adapter;
 
 public class PrintBanner extends Banner implements Print{
+	
 	public PrintBanner(String string) {
 		super(string);
 	}
@@ -14,4 +17,30 @@ public class PrintBanner extends Banner implements Print{
 	public void printStrong() {
 		showWithAster();
 	}
+	
 }
+*/
+
+//委譲によるadapter
+package main.adapter;
+
+public class PrintBanner extends Print{
+	
+	private Banner banner;
+	
+	public PrintBanner(String string) {
+		this.banner = new Banner(string);
+	}
+	
+	@Override
+	public void printWeak() {
+		banner.showWithParen();
+	}
+	
+	@Override
+	public void printStrong() {
+		banner.showWithAster();
+	}
+	
+}
+
