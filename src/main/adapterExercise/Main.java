4760dbd58489c3797d38323cef9d9aaf7ff2ac5c
arohpos <110832649+arohpos@users.noreply.github.com>
2022-09-15 00:05:56 +0900

    fix:  change path to put newfile.txt

--- src/main/adapterExercise/Main.java
+++ src/main/adapterExercise/Main.java
@@ -8,11 +8,11 @@ public class Main {
 
 		FileIO f = new FileProperties();
 		try {
-			f.readFromFile("file.txt");
+			f.readFromFile("/Applications/Eclipse_2022-03.app/Contents/workspace/design-pattern/src/main/adapterExercise/file.txt");
 			f.setValue("width", "1024");
 			f.setValue("height", "512");
 			f.setValue("depth", "32");
-			f.writeToFile("newfile.txt");
+			f.writeToFile("./src/main/adapterExercise/newfile.txt");
 			System.out.println("newfile.txt is created.");
 		}catch (IOException e) {
 			e.printStackTrace();
