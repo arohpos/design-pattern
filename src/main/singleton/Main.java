e05b89104cdb40eb0dad391958e2f001a3c6c23d
arohpos <110832649+arohpos@users.noreply.github.com>
2022-09-21 22:26:06 +0900

    add: add Triple class

--- src/main/singleton/Main.java
+++ src/main/singleton/Main.java
@@ -16,12 +16,37 @@ public class Main {
 		
 		
 		System.out.println("start!!");
-		
 		for(int i = 0; i < 10; i++) {
 			System.out.println(i+1 + ":" +TicketMaker.getInstance().getNextTicketNumber());
 		}
+		System.out.println("end!!");
+
+		System.out.println("start!!");
+		Triple a1 = Triple.getInstance("ALPHA");
+		Triple a2 = Triple.getInstance("ALPHA");
+		Triple b1 = Triple.getInstance("BETA");
+		Triple b2 = Triple.getInstance("BETA");
+		Triple c1 = Triple.getInstance("GAMMA");
+		Triple c2 = Triple.getInstance("GAMMA");
+		if(a1 == a2) {
+			System.out.println("a1 == a2");
+		}else {
+			System.out.println("a1 != a2");			
+		}
+		if(b1 == b2) {
+			System.out.println("b1 == b2");
+		}else {
+			System.out.println("b1 != b2");			
+		}
+		if(c1 == c2) {
+			System.out.println("c1 == c2");
+		}else {
+			System.out.println("c1 != c2");			
+		}
 		
 		System.out.println("end!!");
+		
+		
 		
 	}
 }
